78b3492a3d0735b52ca13a327b8700aedb2d69dc
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2020-12-10 20:42:07 -0800

    Add files via upload

---
+++ 106a_interpolator.py
@@ -0,0 +1,287 @@
+""" Test program to follow a sequence of closely-spaced waypoints """
+
+from pure_pursuit_control import PurePursuitController
+from lane_finder import LaneFinder
+import glob
+import os
+import sys
+import time
+
+# ==============================================================================
+# -- Find CARLA module ---------------------------------------------------------
+# ==============================================================================
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+# ==============================================================================
+# -- Add PythonAPI for release mode --------------------------------------------
+# ==============================================================================
+try:
+    sys.path.append(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__))) + '/carla')
+except IndexError:
+    pass
+
+import carla
+from carla import ColorConverter as cc
+
+from agents.navigation.global_route_planner import GlobalRoutePlanner
+from agents.navigation.global_route_planner_dao import GlobalRoutePlannerDAO
+from agents.navigation.local_planner import RoadOption
+
+import argparse
+import numpy as np
+from queue import Queue
+
+WAYPOINT_ERROR_THRESHOLD = 1  # Error to consider waypoint reached
+PASSENGER_ARRIVAL_RATE = 30  # Expected time between passengers in seconds
+MAP_SAMPLING_RESOLUTION = 10 # Distance between possible waypoints for pickup / dropoff
+
+class PassengerGenerator:
+    def __init__(self, passenger_arrival_rate, rng):
+        self.passenger_arrival_rate = passenger_arrival_rate
+        self.rng = rng
+        self.next_passenger_time = None
+
+    def new_passenger_arrived(self, time):
+        if self.next_passenger_time is None or time >= self.next_passenger_time:
+            self.next_passenger_time = time + \
+                self.rng.exponential(self.passenger_arrival_rate)
+            return True
+        else:
+            return False
+
+    def generate_new_passenger(self, possible_waypoints):
+        return self.rng.choice(possible_waypoints, size=(2), replace=False)
+
+
+class EndpointSelector:
+    def __init__(self):
+        self.passenger_points = Queue()
+        self.current_passenger_point = None
+        self.point = 0
+
+    def add_passenger_point(self, passenger_point):
+        self.passenger_points.put(passenger_point)
+
+    def next_endpoint(self):
+        if self.current_passenger_point is None or self.point == 1:
+            if self.passenger_points.empty():
+                print('Tried to select new passenger but there are no more passengers')
+                return None
+            else:
+                print('Selected new passenger')
+                self.current_passenger_point = self.passenger_points.get()
+                self.point = 0
+        else:
+            print('Moving to drop off passenger')
+            self.point = self.point + 1
+            assert(self.point == 1)
+        return self.current_passenger_point[self.point]
+
+
+class Planner:
+    def __init__(self, world_map, sampling_resolution, waypoint_error_threshold):
+        self.global_route_planner_dao = GlobalRoutePlannerDAO(
+            world_map, 1)
+        self.global_route_planner = GlobalRoutePlanner(
+            self.global_route_planner_dao)
+        self.global_route_planner.setup()
+        self.endpoint = None
+        self.waypoints = None
+        self.road_options = None
+        self.target_waypoint_i = 0
+        self.waypoint_error_threshold = waypoint_error_threshold
+
+    def _remove_extra(route):
+        def road_option(point):
+            return point[1]
+        # never remove last or first waypoint
+        for i in range(len(route) - 2, 0, -1):
+            if road_option(route[i]) == RoadOption.RIGHT or road_option(route[i]) == RoadOption.LEFT:
+                continue
+            if road_option(route[i]) == road_option(route[i-1]) and road_option(route[i]) == road_option(route[i+1]):
+                del route[i]
+
+    def set_endpoint(self, vehicle, endpoint):
+        self.endpoint = endpoint
+        vehicle_location = vehicle.get_transform().location
+        endpoint_location = endpoint.transform.location
+        route = self.global_route_planner.trace_route(
+            vehicle_location, endpoint_location)
+        Planner._remove_extra(route)
+        self.waypoints = [point[0] for point in route]
+        self.road_options = [point[1] for point in route]
+        self.target_waypoint_i = 0
+        print("Set new endpoint")
+        print("Path length: ", len(self.waypoints))
+        print("Path: ")
+        for (w, r) in zip(self.waypoints, self.road_options):
+            print(w.transform.location, r)
+        print("First waypoint: ", self.waypoints[0].transform.location)
+
+    def get_target_waypoint(self, vehicle):
+        vehicle_location = vehicle.get_transform().location
+        target_waypoint = self.waypoints[self.target_waypoint_i]
+        if target_waypoint.transform.location.distance(vehicle_location) < self.waypoint_error_threshold:
+            if self.target_waypoint_i + 1 >= len(self.waypoints):
+                print('No more waypoints')
+                return None
+            self.target_waypoint_i += 1
+            target_waypoint = self.waypoints[self.target_waypoint_i]
+            print('New waypoint (', self.target_waypoint_i, '/',
+                  len(self.waypoints), '): ', target_waypoint.transform.location, ' ', self.road_options[self.target_waypoint_i])
+        return target_waypoint
+
+    def reached_endpoint(self, vehicle):
+        vehicle_location = vehicle.get_transform().location
+        end_waypoint = self.waypoints[-1]
+        if end_waypoint.transform.location.distance(vehicle_location) < self.waypoint_error_threshold:
+            print('Reached endpoint')
+            return True
+        return False
+
+
+def main(args):
+    client = carla.Client(args.host, args.port)
+    client.set_timeout(10.0)
+
+    rng = np.random.default_rng(args.seed)
+
+    vehicle = None
+    lanefinder = None
+    try:
+        world = client.get_world()
+        spectator = world.get_spectator()
+        print('got world')
+
+        # Setup world
+        settings = world.get_settings()
+        # world.set_weather(carla.WeatherParameters())
+        settings.synchronous_mode = True
+        settings.fixed_delta_seconds = 0.05
+        world.apply_settings(settings)
+        world.tick() # check if necessary
+        print('set world settings')
+
+        # Select and spawn vehicle.
+        vehicle_blueprint = rng.choice(
+            world.get_blueprint_library().filter(args.filter))
+        
+        world_map = world.get_map()
+        spawn_point = world_map.get_spawn_points()[12]
+        vehicle = world.try_spawn_actor(vehicle_blueprint, spawn_point)
+        print('created vehicle')
+        
+        # Move spectator to newly created vehicle.
+        world.tick()
+        world_snapshot = world.wait_for_tick()
+        actor_snapshot = world_snapshot.find(vehicle.id)
+        spectator.set_transform(actor_snapshot.get_transform())
+        print("Initial vehicle position: ", vehicle.get_transform().location)
+
+        all_waypoints = world_map.generate_waypoints(10) # meters between pickup/dropoff positions 
+
+        passenger_gen = PassengerGenerator(PASSENGER_ARRIVAL_RATE, rng)
+
+        endpoint_selector = EndpointSelector()
+        endpoint_selector.add_passenger_point(
+            passenger_gen.generate_new_passenger(all_waypoints))
+        current_endpoint = endpoint_selector.next_endpoint()
+        assert(current_endpoint is not None)
+        print(current_endpoint.transform.location)
+
+        planner = Planner(world_map, MAP_SAMPLING_RESOLUTION,
+                          WAYPOINT_ERROR_THRESHOLD)
+        planner.set_endpoint(vehicle, current_endpoint)
+
+        controller = PurePursuitController(vehicle, target_speed=25)
+        print('Created controller')
+
+        lanefinder = LaneFinder(world, vehicle)
+        print('Created Lane Finder')
+
+        world.tick()
+        while True:
+            print('a')
+            world_snapshot = world.wait_for_tick()
+            print('b')
+            time = world_snapshot.timestamp.elapsed_seconds
+            print('time: start')
+            if passenger_gen.new_passenger_arrived(time):
+                print("New passenger at time ", time)
+                endpoint_selector.add_passenger_point(
+                    passenger_gen.generate_new_passenger(all_waypoints))
+
+            target_waypoint = planner.get_target_waypoint(vehicle)
+            while target_waypoint is None:
+                current_endpoint = endpoint_selector.next_endpoint()
+                if current_endpoint is None:
+                    target_waypoint = world_map.get_waypoint(
+                        vehicle.get_transform().location)
+                    break
+                planner.set_endpoint(vehicle, current_endpoint)
+                target_waypoint = planner.get_target_waypoint(vehicle)
+            print('time: ', time, ' handled passengers and waypoints')
+            control = controller.run_step(vehicle, target_waypoint)
+            vehicle.apply_control(control)
+            print('time: ', time, ' applied control')
+            lanefinder.run_step()
+            print('time: ', time, ' ran lanefinder')
+    finally:
+        # Destroy vehicle.
+        print("Destroy Vehicle")
+        # client.apply_batch([carla.command.DestroyActor(vehicle),
+        #     carla.command.DestroyActor(lanefinder.sensor)])
+        vehicle.destroy()
+        lanefinder.sensor.destroy()
+
+if __name__ == '__main__':
+    try:
+        argparser = argparse.ArgumentParser(description=__doc__)
+        argparser.add_argument(
+            '--host',
+            metavar='H',
+            default='127.0.0.1',
+            help='IP of the host server (default: 127.0.0.1)')
+        argparser.add_argument(
+            '-p', '--port',
+            metavar='P',
+            default=2000,
+            type=int,
+            help='TCP port to listen to (default: 2000)')
+        argparser.add_argument(
+            '--filter',
+            metavar='PATTERN',
+            default='vehicle.carlamotors.carlacola',
+            help='vehicles filter (default: vehicle.carlamotors.carlacola)')
+        argparser.add_argument(
+            '--tm-port',
+            metavar='P',
+            default=8000,
+            type=int,
+            help='port to communicate with TM (default: 8000)')
+        argparser.add_argument(
+            '--sync',
+            action='store_true',
+            help='Synchronous mode execution')
+        argparser.add_argument(
+            '--hybrid',
+            action='store_true',
+            help='Enanble')
+        argparser.add_argument(
+            '-s', '--seed',
+            metavar='S',
+            type=int,
+            help='Random device seed')
+        args = argparser.parse_args()
+        main(args)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('\ndone.')
---
+++ lane_finder.py
@@ -0,0 +1,179 @@
+import glob
+import os
+import sys
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+import carla
+
+import random
+import time
+import numpy as np
+import cv2
+import queue
+
+
+class LaneFinder:
+    im_width = 640
+    im_height = 480
+    new_factor = 0.5
+
+    def __init__(self, world, vehicle):
+        self.world = world
+        self.vehicle = vehicle
+
+        self.tMatrix = np.array([[1, 0, 0, 0],
+                                 [0, 1, 0, 0],
+                                 [0, 0, 1, 0],
+                                 [0, 0, 0, 1]], np.float32)
+
+        self.kf = cv2.KalmanFilter(4, 4)
+        self.kf.transitionMatrix = self.tMatrix
+        self.kf.measurementMatrix = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ], np.float32)
+        self.kf.processNoiseCov = cv2.setIdentity(self.kf.processNoiseCov, 1e-2)
+        self.kf.measurementNoiseCov = cv2.setIdentity(
+            self.kf.measurementNoiseCov, 1e-1)
+        self.kf.errorCovPost = cv2.setIdentity(self.kf.errorCovPost, 1e-1)
+
+        print(self.kf.predict())
+
+        self.left_x = 0
+        self.right_x = 0
+
+        # Setup sensor
+        blueprint_library = world.get_blueprint_library()
+        # https://carla.readthedocs.io/en/latest/cameras_and_sensors
+        # get the blueprint for this sensor
+        self.blueprint = blueprint_library.find('sensor.camera.rgb')
+        print(self.blueprint)
+        # change the dimensions of the image
+        self.blueprint.set_attribute('image_size_x', f'{self.im_width}')
+        self.blueprint.set_attribute('image_size_y', f'{self.im_height}')
+        self.blueprint.set_attribute('fov', '110')
+
+        # Adjust sensor relative to vehicle
+        self.spawn_point = carla.Transform(carla.Location(x=2.5, z=0.7))
+
+        # spawn the sensor and attach to vehicle.
+        self.sensor = world.spawn_actor(self.blueprint, self.spawn_point, attach_to=self.vehicle)
+        print('sensor: ', sensor)
+        self.image_queue = queue.Queue()
+        self.sensor.listen(self.image_queue.put)
+
+    def process_img(self, image):
+        i = np.array(image.raw_data)
+        i2 = i.reshape((self.im_height, self.im_width, 4))
+        img = np.copy(i2[:, :, :3])
+        # cv2.imshow("", img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(img, (5, 5), 0)
+        cv2.imshow("b", blur)
+        canny = cv2.Canny(blur, 30, 150)
+        masked = cv2.bitwise_and(canny, self.region_of_interest(canny))
+        lanes = self.line_image(masked)
+        final = cv2.addWeighted(masked, 0.8, lanes, 1, 1)
+        cv2.imshow("f", final)
+        cv2.waitKey(1)
+
+    def region_of_interest(self, image):
+        margin = 200
+        poly = np.array([
+            [(margin, self.im_height//2), (0, 9*self.im_height//16), (0, self.im_height), (self.im_width,
+                                                                                           self.im_height), (self.im_width, 9*self.im_height//16), (self.im_width-margin, self.im_height//2)]
+        ])
+        # poly = np.array([
+        # 	[(0, im_height//2), (0, im_height), (im_width, im_height), (im_width-margin, im_height//2)]
+        # 	])
+        mask = np.zeros_like(image)
+        cv2.fillPoly(mask, poly, 255)
+        return mask
+
+    def line_image(self, image):
+        lanes = np.zeros_like(image)
+        lines = cv2.HoughLinesP(image, 2, np.pi/180, 70,
+                                minLineLength=40, maxLineGap=5)
+
+        left = []
+        right = []
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                if x1 == x2 or abs(np.arctan(abs(y2-y1)/abs(x2-x1))) >= np.pi/36:
+                    m, b = np.polyfit((x1, x2), (y1, y2), 1)
+                    if m < 0:
+                        left.append((m, b))
+                    else:
+                        right.append((m, b))
+            left_avg = np.average(left, axis=0)
+            right_avg = np.average(right, axis=0)
+
+            def make_coords(image, params):
+                (m, b) = params
+                y1 = image.shape[0]
+                y2 = int(y1*3/5)
+                x1 = int((y1-b)/(m+.00001))
+                x2 = int((y2-b)/(m+.00001))
+                return np.array([x1, y1, x2, y2])
+
+            def left_coord(params):
+                (m, b) = params
+                return int((y1-b)/(m+.00001))
+
+            def right_coord(params):
+                (m, b) = params
+                return int((y2-b)/(m+.00001))
+
+            if not np.any(np.isnan(left_avg)) and not np.any(np.isnan(right_avg)):
+                measurements = np.array(
+                    list(left_avg) + list(right_avg), dtype=np.float32)
+                left_measure = make_coords(image, left_avg)
+                right_measure = make_coords(image, right_avg)
+                # print(abs(right_measure[2]-left_measure[2]))
+                if 200 <= abs(right_measure[2]-left_measure[2]) <= 400:
+                    self.kf.correct(measurements)
+
+            prediction = self.kf.predict()
+            left_line = make_coords(image, prediction[:2])
+            right_line = make_coords(image, prediction[2:])
+
+            for x1, y1, x2, y2 in [left_line, right_line]:
+                cv2.line(lanes, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+            self.left_x = (1-self.new_factor) * left_x + \
+                self.new_factor * left_coord(prediction[:2])
+            self.right_x = (1-self.new_factor) * right_x + \
+                self.new_factor * right_coord(prediction[2:])
+
+            print('dists: ', self.left_x, ', ', self.right_x)
+        return lanes
+
+    def average_slope(self, image, lines):
+        left = []
+        right = []
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            m, b = np.polyfit((x1, x2), (y1, y2), 1)
+            if m < 0:
+                left.append((m, b))
+            else:
+                right.append((m, b))
+        left_avg = np.average(left, axis=0)
+        right_avg = np.average(right, axis=0)
+
+
+    def process_img2(image):
+        print(
+            f"Obstacle: {image.other_actor} is {image.distance} units away from {image.actor}.")
+
+    def run_step(self):
+        if not self.image_queue.empty():
+            self.process_img(self.image_queue.get())
